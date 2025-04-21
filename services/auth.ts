b616472b4dea8d4be39e4dc879483ae00f213a35
atan241200 <atan241200@gmail.com>
2025-04-21 10:14:14 +0700

    update status profile and func Logout

--- services/auth.ts
+++ services/auth.ts
@@ -1,12 +1,11 @@
 import api from "@/config/api";
-import { LoginEmailType, LoginPhoneType, LoginType,RegisterType } from "@/types/user";
+import { LoginEmailType, LoginByPhone, LoginType,RegisterType } from "@/types/user";
 
 const authApi = {
   login: (formData: LoginType) => api.post("/login", formData),
   loginEmail: (formData: LoginEmailType) => api.post("/LoginByEmail", formData),
-  loginPhone: (formData: LoginPhoneType) => api.post("/LoginPhone", formData),
+  loginPhone: (formData: LoginByPhone) => api.post("/LoginByPhone", formData),
   register: (formData: RegisterType) => api.post("Accounts/Resgiter", formData),
-
 
 };
 
